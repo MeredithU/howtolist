64a689931458f0dab994bf0ef5d023af2455b03e
jzhang20133 <jzhang906@gmail.com>
2013-07-21 01:01:01 -0700

    create new rails app howtolist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howtolist::Application.config.secret_token = 'd48eabc8064ab1420ea8feea7e3d0686031a0b3f8138d155a2f6b5d9c5aed871e071bee176fc88d119744053a878acda504ddfa7bb01852e5fedf2d11ab70fe9'
